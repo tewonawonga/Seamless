f7e991aedd9874cfe58f605d0f4c90f258047832
James <james@seamlessthemes.com>
2013-08-14 22:44:20 -0400

    Adding back sass

---
+++ config.rb
@@ -0,0 +1,14 @@
+# compass config.rb refrences - http://compass-style.org/help/tutorials/configuration-reference/
+
+# path to file structures
+css_dir = "." # css generate to project root
+sass_dir = "." # sass folder is in project root too
+images_dir = "images" # please do not remove this line
+javascripts_dir = "js"
+require "susy"
+
+# output_style options - :nested, :expanded, :compact, :compressed
+output_style = :expanded
+
+# line_comments options - true, false
+line_comments = false
