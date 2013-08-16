8dd3bd832907a5ffc7dab1c221d20734c515cb99
James <james@seamlessthemes.com>
2013-08-16 18:52:59 -0400

    Adding config.rb

--- config.rb
+++ config.rb
@@ -5,10 +5,11 @@ css_dir = "." # css generate to project root
 sass_dir = "." # sass folder is in project root too
 images_dir = "images" # please do not remove this line
 javascripts_dir = "js"
-require "susy"
-
+    
 # output_style options - :nested, :expanded, :compact, :compressed
 output_style = :expanded
 
 # line_comments options - true, false
 line_comments = false
+line_comments = true # by Compass.app 
+sass_options = {:debug_info=>false} # by Compass.app 
